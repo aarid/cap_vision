4545c142db94faa364a5be8f852831127ff8129c
aaridouane <aaridouane@gmail.com>
2025-01-24 10:10:38 +0100

    add cmake with explicit paths and main file

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+// #include "ui/main_window.hpp"
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    
+    // capvision::ui::MainWindow mainWindow;
+    // mainWindow.show();
+    
+    return app.exec();
+}
